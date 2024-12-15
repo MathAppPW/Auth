6ecdb4f818d9e6884a370f2b0fde621713d7161e
Kamil <01178868@pw.edu.pl>
2024-12-15 21:16:16 +0100

    reflection

--- Auth/Program.cs
+++ Auth/Program.cs
@@ -2,11 +2,13 @@ using Auth.Dal.Interfaces;
 using Auth.Helpers;
 using Auth.Services;
 using Microsoft.EntityFrameworkCore;
+using Grpc.AspNetCore.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var connectionString = "Server=mathapp-tests.postgres.database.azure.com;Database=postgres;Port=5432;User Id=mathapp;Password=projektZespolowy123;Ssl Mode=Require;";
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -18,6 +20,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<AuthenticatorService>();
+app.MapGrpcReflectionService();
+
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
